0044528c03ae8fb767ebd2d28c09e49145a2673b
Jean Luc Habimana <133254246+Jeanluchabi@users.noreply.github.com>
2024-06-16 22:36:29 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function betNow() {
+    alert('Betting functionality is not implemented yet.');
+    // Redirecting to a premier bet Rwanda site.
+    window.location.href = 'https://www.premierbet.com/rw/';
+}
